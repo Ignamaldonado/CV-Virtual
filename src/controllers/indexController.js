d7ea3ca43b111cee5b2ce85e9254c4f2ca1843bb
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-04-11 12:46:02 -0300

    Terminada las funcionalidades de la pagina

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const {validationResult} = require('express-validator')
 
 const userFilePath = path.join(__dirname, '../data/user.JSON');
-const user = JSON.parse(fs.readFileSync(userFilePath, 'utf8'));
+var user = JSON.parse(fs.readFileSync(userFilePath, 'utf8'));
 
 const controller = {
     index: (req,res) => {
@@ -19,14 +19,14 @@ const controller = {
         let logged;
         let errors = validationResult(req)
         if (errors.isEmpty()) {
-        if (user.email == req.body.email) {
+        if (user.email.toLowerCase() == req.body.email.toLowerCase()) {
             if (bcrypt.compareSync(req.body.password , user.password)) {
-            logged = user
+            logged = user.email
             req.session.logged = logged
             res.render('index' , {user , loggedUser : req.session.logged})
         } else {
             let changepassword = true
-            res.render('login' , {errors: [{msg: "Contrasenia invalida"}] , old: req.body , changepassword})
+            res.render('login' , {errors: [{msg: "Contrasenia invalida" , old: req.body}] , old: req.body , changepassword})
         }
         } else {
             res.render('login' , {errors: [{msg: "Email invalido, prueba ignacio.maldonado96@gmail.com"}] , old: req.body})
@@ -53,7 +53,36 @@ const controller = {
 
     editview: (req,res) => {
         res.render('edit' , {user})
-    }
+    },
+
+    editput: (req,res) => {
+            let {name, position, about, phone, location, HTML, CSS, Bootstrap, JavaScript, Python, mySQL, jobtitle, jobdate, jobsubtitle, jobcomment, studytitle1, studydate1, studysubtitle1, studytitle2, studydate2, studysubtitle2} = req.body
+            let avatar;
+            let email;
+            let web;
+            let password;
+            if (jobtitle == '') {
+                jobdate = ''
+                jobsubtitle = ''
+                jobcomment = ''
+            }
+            if (req.file) {
+                avatar = "/img/avatars/" + req.file.filename
+                password = user.password
+                email = req.body.email.toLowerCase()
+                web = req.body.web.toLowerCase()
+                user = {name, position, avatar, about, phone, email, web, location, HTML, CSS, Bootstrap, JavaScript, Python, mySQL, password, jobtitle, jobdate, jobsubtitle, jobcomment, studytitle1, studydate1, studysubtitle1, studytitle2, studydate2, studysubtitle2}
+            } else {
+                password = user.password
+                avatar = user.avatar
+                email = req.body.email.toLowerCase()
+                web = req.body.web.toLowerCase()
+                user = {name, position, avatar, about, phone, email, web, location, HTML, CSS, Bootstrap, JavaScript, Python, mySQL, password, jobtitle, jobdate, jobsubtitle, jobcomment, studytitle1, studydate1, studysubtitle1, studytitle2, studydate2, studysubtitle2}
+            }
+            let editedUser = JSON.stringify (user)
+            fs.writeFileSync (userFilePath , editedUser)
+            res.render ('index' , {user, loggedUser : req.session.logged})
+        }
 }
 
 module.exports = controller;
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express'); // llamado express
 const controller = require ('../controllers/indexController'); 
-const {check} = require('express-validator');// llamado controllers
+const {check} = require('express-validator');
+const multer = require('multer');
+const path = require('path');// llamado controllers
 
 const auth = require('../middleware/authmiddleware')
 const guest = require('../middleware/guestmiddleware')
@@ -8,14 +10,26 @@ const guest = require('../middleware/guestmiddleware')
 const validator = [
     check('email')
         .notEmpty()
-        .withMessage('capo te falto un email')
+        .withMessage('Tienes que completar el campo de email')
         .bail()
         .isEmail()
-        .withMessage('alto tonto no sabes ni poner un email'),
+        .withMessage('Tienes que completar con un email valido'),
     check('password')
         .notEmpty()
-        .withMessage('Tienes que ingresar una contrasenia')
+        .withMessage('Tienes que completar el campo de contraseña')
 ]   
+
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        let imgFolder = path.join(__dirname, '../../public/img/avatars')
+       cb(null, imgFolder); 
+    },
+    filename: function(req,file,cb){
+        let fileName = file.fieldname + Date.now() + path.extname(file.originalname) 
+       cb(null, fileName);
+    }
+})
+var uploadFile = multer({storage: storage})
 
 const router = express.Router(); // parametro enrutador de express 
 
@@ -24,11 +38,11 @@ router.get('/', controller.index); // rooteo de get
 router.get('/login' , guest , controller.login)
 router.post('/login', validator , controller.auth)
 
-router.get('/password' , controller.passchange)
+router.get('/password' , guest , controller.passchange)
 router.put('/password' , controller.passwordput)
 
 router.get('/edit', auth , controller.editview)
-
+router.put('/edit', auth , uploadFile.single('avatar') , controller.editput)
 /*router.get('/check', (req,res) => {
     if (req.session.logged == undefined) {
         res.send('no te logueaste papu')
